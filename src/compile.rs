b8a3b6be864449dd6d79825e743c6583319d1c55
Max Gilmour <mgilmour@maxgmr.ca>
2025-11-21 00:41:54 +0100

    Make compile stub produce empty ASM

--- src/compile.rs
+++ src/compile.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     io::{self, Write},
     process::Command,
 };
@@ -8,6 +9,12 @@ use color_eyre::eyre::eyre;
 
 const ASSEMBLY_EXTENSION: &str = ".s";
 
+const ASM_STUB: &str = ".globl main
+main:
+    xor %eax, %eax
+    ret
+";
+
 /// Currently a stub.
 pub fn compile(preprocessed_path: &Utf8Path) -> color_eyre::Result<Utf8PathBuf> {
     let mut output_path = preprocessed_path.to_owned();
@@ -16,9 +23,7 @@ pub fn compile(preprocessed_path: &Utf8Path) -> color_eyre::Result<Utf8PathBuf> 
     }
 
     // TODO
-    let output = Command::new("touch").arg(&output_path).output()?;
-    io::stdout().write_all(&output.stdout)?;
-    io::stderr().write_all(&output.stderr)?;
+    fs::write(&output_path, ASM_STUB)?;
 
     let output = Command::new("rm").arg(preprocessed_path).output()?;
     io::stdout().write_all(&output.stdout)?;
