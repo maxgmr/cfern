0d7bf7fde5c8c4da59152cc06a9809ee2d3c6297
Max Gilmour <mgilmour@maxgmr.ca>
2025-11-20 20:55:27 +0100

    Impl compiler driver CLI framework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+//! # cfern
+//! A basic `x86_64` C compiler.
+#![cfg(all(target_os = "linux", target_arch = "x86_64"))]
+#![warn(
+    missing_docs,
+    missing_debug_implementations,
+    rust_2018_idioms,
+    clippy::all,
+    clippy::pedantic,
+    clippy::todo
+)]
+
+use std::{
+    fs,
+    io::{self, Read},
+};
+
+use camino::Utf8PathBuf;
+use clap::Parser;
+use color_eyre::eyre;
+
+mod parse_cli;
+
+use parse_cli::Cli;
+
+fn main() -> eyre::Result<()> {
+    color_eyre::install()?;
+    let cli = Cli::parse();
+
+    let input = read_input_file(cli.input_file.as_ref())?;
+
+    Ok(())
+}
+
+fn read_input_file(input_file: Option<&Utf8PathBuf>) -> io::Result<String> {
+    match input_file {
+        Some(path) => fs::read_to_string(path),
+        None => read_stdin(),
+    }
+}
+
+fn read_stdin() -> io::Result<String> {
+    let mut bytes = Vec::new();
+    let stdin = io::stdin();
+    let mut handle = stdin.lock();
+    handle.read_to_end(&mut bytes)?;
+    String::from_utf8(bytes).map_err(|e| {
+        let s = e.to_string();
+        io::Error::new(io::ErrorKind::InvalidInput, s)
+    })
 }
---
+++ src/parse_cli.rs
@@ -0,0 +1,34 @@
+use camino::Utf8PathBuf;
+use clap::{ArgAction, ArgGroup, Parser};
+
+#[derive(Clone, Debug, Parser)]
+#[clap(
+    author = "Max Gilmour", 
+    about = "A basic x86_64 C compiler",
+    group(
+        ArgGroup::new("run_step")
+            .required(false)
+            .args(["lex", "parse", "codegen", "assembly"])
+    )
+)]
+#[allow(clippy::struct_excessive_bools)]
+pub struct Cli {
+    /// Run the lexer, but stop before parsing.
+    #[clap(short = 'L', long)]
+    pub lex: bool,
+
+    /// Run the lexer and parser, but stop before assembly generation.
+    #[clap(short = 'P', long)]
+    pub parse: bool,
+
+    /// Perform lexing, parsing, and assembly generation, but stop before code emission.
+    #[clap(short = 'C', long)]
+    pub codegen: bool,
+
+    /// Emit an assembly file without assembling or linking it.
+    #[clap(short = 'S', long)]
+    pub assembly: bool,
+
+    /// The C file to compile. Leave empty to accept stdin.
+    pub input_file: Option<Utf8PathBuf>,
+}
