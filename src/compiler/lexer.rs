1fae2e81008b0827d6df4ba45ab46d296e41b083
Max Gilmour <mgilmour@maxgmr.ca>
2025-11-22 01:06:48 +0100

    Impl lexer error display

--- src/compiler/lexer.rs
+++ src/compiler/lexer.rs
@@ -1,4 +1,12 @@
+use std::fmt::Display;
+
 use crate::compiler::token::{Token, get_next_token};
+
+use color_eyre::{
+    Section, SectionExt,
+    eyre::{OptionExt, eyre},
+    owo_colors::OwoColorize,
+};
 
 /// Lex a string of valid C code into a list of [`Token`]s.
 pub fn lex(_data: &str) -> color_eyre::Result<Vec<Token>> {
@@ -19,5 +27,29 @@ impl<'a> Lexer<'a> {
             original: data,
             remaining: data,
         }
+    }
+
+    /// Return an error showing where in the file the error occurred.
+    fn lexing_error<S: AsRef<str> + Display>(
+        &self,
+        message: S,
+    ) -> color_eyre::Result<Option<Token<'_>>> {
+        let consumed_chars = self.original.len() - self.remaining.len();
+        let consumed_lines: Vec<&str> = self.original[..consumed_chars].split("\n").collect();
+        let line_num = consumed_lines.len();
+        let consumed_line = consumed_lines.last().ok_or_eyre("no consumed lines")?;
+        let col_num = consumed_line.len() + 1;
+        let remaining_line: &str = self.remaining.split("\n").next().unwrap_or("");
+        Err(eyre!("{}", message))
+            .with_section(|| format!("{}:{}", line_num.blue(), col_num.blue()).header("Line info:"))
+            .with_section(|| {
+                format!(
+                    "{}{}\n{}{}",
+                    consumed_line,
+                    remaining_line,
+                    (0..(col_num - 1)).map(|_| " ").collect::<String>(),
+                    "^ Here".bright_red().bold()
+                )
+            })
     }
 }
