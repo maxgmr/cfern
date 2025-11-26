6e16ab54c7475df37615425e2b8e7649fcaee0cb
Max Gilmour <mgilmour@maxgmr.ca>
2025-11-27 00:28:31 +0100

    Impl prototype assembly generator

--- src/compiler/asm_generator.rs
+++ src/compiler/asm_generator.rs
@@ -1,8 +1,8 @@
-use crate::compiler::parser::CProgram;
+use crate::compiler::parser::{self, CProgram};
 
 /// Generates an assembly abstract syntax tree from a [`CProgram`] abstract syntax tree.
-pub fn generate_asm<'a>(data: &'a str, ast: &CProgram<'_>) -> color_eyre::Result<AsmProgram<'a>> {
-    todo!()
+pub fn generate_asm<'a>(ast: &CProgram<'a>) -> Result<AsmProgram<'a>, AsmGenerationError> {
+    parse_program(ast)
 }
 
 /// The root node of an ASM abstract syntax tree. Represents an ASM program.
@@ -34,6 +34,19 @@ pub enum Operand {
     Register(Register),
     ImmediateInt(isize),
 }
+impl Operand {
+    fn try_parse_imm_int(expr: &parser::Expression<'_>) -> Result<Self, AsmGenerationError> {
+        match expr {
+            parser::Expression::Constant(s) => {
+                Ok(Self::ImmediateInt(s.parse::<isize>().map_err(|_| {
+                    AsmGenerationError::IntParseError {
+                        input: s.to_string(),
+                    }
+                })?))
+            }
+        }
+    }
+}
 
 /// Represents a particular register used as an operand within an ASM abstract syntax tree.
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
@@ -43,4 +56,43 @@ pub enum Register {
 
 /// An error which can occur whilst parsing a [`CProgram`].
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub enum AsmGenerationError {}
+pub enum AsmGenerationError {
+    IntParseError { input: String },
+}
+impl std::fmt::Display for AsmGenerationError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            AsmGenerationError::IntParseError { input } => {
+                write!(f, "failed to parse string \"{input}\" as int")
+            }
+        }
+    }
+}
+impl std::error::Error for AsmGenerationError {}
+
+fn parse_program<'a>(ast: &CProgram<'a>) -> Result<AsmProgram<'a>, AsmGenerationError> {
+    Ok(AsmProgram {
+        function: parse_function(&ast.function)?,
+    })
+}
+
+fn parse_function<'a>(function: &parser::Function<'a>) -> Result<Function<'a>, AsmGenerationError> {
+    Ok(Function {
+        name: function.name,
+        instructions: parse_instructions(&function.body)?,
+    })
+}
+
+fn parse_instructions(
+    body: &parser::Statement<'_>,
+) -> Result<Vec<Instruction>, AsmGenerationError> {
+    match body {
+        parser::Statement::Return(expr) => Ok(vec![
+            Instruction::Mov {
+                source: Operand::try_parse_imm_int(expr)?,
+                destination: Operand::Register(Register::Eax),
+            },
+            Instruction::Ret,
+        ]),
+    }
+}
