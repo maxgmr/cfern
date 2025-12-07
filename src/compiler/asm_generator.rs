6b33cf2ac64b6ea628f168e93deb77f6944c1b36
Max Gilmour <mgilmour@maxgmr.ca>
2025-12-07 18:13:48 +0100

    Impl Display for asm types

--- src/compiler/asm_generator.rs
+++ src/compiler/asm_generator.rs
@@ -17,6 +17,11 @@ pub struct Function<'a> {
     pub name: &'a str,
     pub instructions: Vec<Instruction>,
 }
+impl<'a> Function<'a> {
+    pub fn name_string_asm(&self) -> String {
+        format!("\t.globl {}\n{}:", self.name, self.name)
+    }
+}
 
 /// Represents an ASM instruction within an ASM abstract syntax tree.
 #[derive(Clone, Debug, PartialEq, Eq)]
@@ -26,6 +31,19 @@ pub enum Instruction {
         destination: Operand,
     },
     Ret,
+}
+impl std::fmt::Display for Instruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use Instruction::*;
+
+        match self {
+            Mov {
+                source,
+                destination,
+            } => write!(f, "\tmovl {source}, {destination}"),
+            Ret => write!(f, "\tret"),
+        }
+    }
 }
 
 /// Represents an operand used in an ASM instruction within an ASM abstract syntax tree.
@@ -47,11 +65,30 @@ impl Operand {
         }
     }
 }
+impl std::fmt::Display for Operand {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use Operand::*;
+
+        match self {
+            Register(r) => r.fmt(f),
+            ImmediateInt(i) => write!(f, "${i}"),
+        }
+    }
+}
 
 /// Represents a particular register used as an operand within an ASM abstract syntax tree.
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum Register {
     Eax,
+}
+impl std::fmt::Display for Register {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use Register::*;
+
+        match self {
+            Eax => write!(f, "%eax"),
+        }
+    }
 }
 
 /// An error which can occur whilst parsing a [`CProgram`].
@@ -94,5 +131,33 @@ fn parse_instructions(
             },
             Instruction::Ret,
         ]),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn display_reg() {
+        assert_eq!(Operand::Register(Register::Eax).to_string(), "%eax");
+    }
+
+    #[test]
+    fn display_imm() {
+        assert_eq!(Operand::ImmediateInt(-4).to_string(), "$-4");
+        assert_eq!(Operand::ImmediateInt(256).to_string(), "$256");
+    }
+
+    #[test]
+    fn display_mov() {
+        assert_eq!(
+            Instruction::Mov {
+                source: Operand::ImmediateInt(1234),
+                destination: Operand::Register(Register::Eax)
+            }
+            .to_string(),
+            "\tmovl $1234, %eax"
+        );
     }
 }
