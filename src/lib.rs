138c22ffb936fb84ba97b00ec7af905a53720137
Max Gilmour <mgilmour@maxgmr.ca>
2025-12-14 18:17:04 +0100

    Add library-wide lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+//! Library crate for the `cfern` C compiler
+
+#![warn(
+    missing_docs,
+    missing_debug_implementations,
+    rust_2018_idioms,
+    clippy::all,
+    clippy::pedantic,
+    clippy::unwrap_used,
+    clippy::todo
+)]
+
 pub mod assemble_and_link;
 pub mod compiler;
 pub mod parse_cli;
